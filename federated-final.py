c2d18e524ebe1d544cd2346a6602e164a5950a11
Anton Akusok <akusok.a@gmail.com>
2023-11-12 12:07:25 +0200

    Plot extended results with noise

--- federated-final.py
+++ federated-final.py
@@ -16,13 +16,11 @@
 # %% editable=true slideshow={"slide_type": ""}
 import numpy as np
 import pandas as pd
-# import pygwalker as pig
 from matplotlib import pyplot as plt
 
 from sklearn.preprocessing import RobustScaler
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 
 from client import ClientNoiseBoth
 
@@ -123,7 +121,7 @@ r2_c3 = get_optimal_performance(c3, 300)
 
 
 # %% [markdown]
-# ## Extend single client performance with more data
+# ## Extend single client with NOISY data
 
 # %%
 def get_optimal_performance_extended(client, collab_client, batch_size=100):
@@ -156,19 +154,44 @@ def get_optimal_performance_extended(client, collab_client, batch_size=100):
 
 
 # %%
-r2_c1_c2 = get_optimal_performance_extended(c1, c2, 50)
-r2_c1_c3 = get_optimal_performance_extended(c1, c3, 200)
-r2_c2_c3 = get_optimal_performance_extended(c2, c3, 300)
+def get_curves(noise_h=0.1, noise_y=0.1):    
+    # reset
+    c1, c2, c3 = (ClientNoiseBoth(i, scaler, records) for i in (1,2,3))
+    for c in (c1, c2, c3):
+        c.init_elm(L, W, bias)
+        c.noise_H = 0
+        c.noise_Y = 0
+
+    r2_c1 = get_optimal_performance(c1, 5)
+    r2_c2 = get_optimal_performance(c2, 50)
+    r2_c3 = get_optimal_performance(c3, 300)
+
+    # add noise
+    c3.noise_H = noise_h
+    c3.noise_Y = noise_y
+    r2_c1_c3 = get_optimal_performance_extended(c1, c3, 200)
+    r2_c2_c3 = get_optimal_performance_extended(c2, c3, 300)
+    
+    c2.noise_H = noise_h
+    c2.noise_Y = noise_y
+    r2_c1_c2 = get_optimal_performance_extended(c1, c2, 50)
+
+    return r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3
+
 
 # %%
-plt.plot(np.arange(1, len(r2_c1)+1)*5, r2_c1, '-b')
-plt.plot(np.arange(0, len(r2_c1_c2))*50 + 100, r2_c1_c2, '--b')
-plt.plot(np.arange(0, len(r2_c1_c3))*200 + 100, r2_c1_c3, '--b')
+results = [get_curves(0.1, 0.1) for _ in range(10)]
+r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3 =  (np.mean([np.array(r[i]) for r in results], axis=0) for i in range(6))
+
+# %%
+plt.plot(np.arange(1, len(r2_c1)+1)*5, r2_c1)
+plt.plot(np.arange(0, len(r2_c1_c2))*50 + 100, r2_c1_c2, '--', c="orange")
+plt.plot(np.arange(0, len(r2_c1_c3))*200 + 100, r2_c1_c3, '--', c="red")
 
 plt.plot(np.arange(1, len(r2_c2)+1)*50, r2_c2, c="orange")
-plt.plot(np.arange(0, len(r2_c2_c3))*300 + 1000, r2_c2_c3, '--', c="orange")
+plt.plot(np.arange(0, len(r2_c2_c3))*300 + 1000, r2_c2_c3, '--', c="red")
 
-plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3)
+plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3, c="red")
 
 plt.plot([0, 10000], [0, 0], '-k')
 plt.ylim([-0.2, 0.8])
