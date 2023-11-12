86b7b26b891c5efaa507c299590236c0712aafcd
Anton Akusok <akusok.a@gmail.com>
2023-11-12 15:41:16 +0200

    Comparison with native noisy client

--- federated-final.py
+++ federated-final.py
@@ -154,11 +154,20 @@ def get_optimal_performance_extended(client, collab_client, batch_size=100):
 
 
 # %%
-def get_curves(noise_h=0.1, noise_y=0.1):    
-    # reset
+clients = []
+for _ in range(30):
     c1, c2, c3 = (ClientNoiseBoth(i, scaler, records) for i in (1,2,3))
     for c in (c1, c2, c3):
         c.init_elm(L, W, bias)
+        c.noise_H = 0
+        c.noise_Y = 0
+    clients.append((c1, c2, c3))
+
+
+# %%
+def get_curves(c1, c2, c3, noise_h=0.1, noise_y=0.1):    
+    # reset noise
+    for c in (c1, c2, c3):
         c.noise_H = 0
         c.noise_Y = 0
 
@@ -179,8 +188,8 @@ def get_curves(noise_h=0.1, noise_y=0.1):
     return r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3
 
 
-# %%
-results = [get_curves(0, 0) for _ in range(10)]
+# %% jupyter={"outputs_hidden": true}
+results = [get_curves(c1, c2, c3, 0, 0) for c1,c2,c3 in clients]
 r2c_c1, r2c_c2, r2c_c3, r2c_c1_c2, r2c_c1_c3, r2c_c2_c3 =  (np.mean([np.array(r[i]) for r in results], axis=0) for i in range(6))
 
 # %%
@@ -201,8 +210,8 @@ plt.show()
 # %% [markdown]
 # ## Extend single client with NOISY data
 
-# %%
-results = [get_curves(0.1, 0.1) for _ in range(10)]
+# %% jupyter={"outputs_hidden": true}
+results = [get_curves(c1, c2, c3, 0.1, 0.1) for c1,c2,c3 in clients]
 r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3 =  (np.mean([np.array(r[i]) for r in results], axis=0) for i in range(6))
 
 # %%
@@ -221,3 +230,73 @@ plt.xscale("log")
 plt.show()
 
 # %%
+
+# %% jupyter={"outputs_hidden": true}
+results2 = [get_curves(c1, c2, c3, 0.15, 0.15) for c1,c2,c3 in clients]
+r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3 =  (np.mean([np.array(r[i]) for r in results2], axis=0) for i in range(6))
+
+# %%
+plt.plot(np.arange(1, len(r2_c1)+1)*5, r2_c1)
+plt.plot(np.arange(0, len(r2_c1_c2))*50 + 100, r2_c1_c2, '--', c="orange")
+plt.plot(np.arange(0, len(r2_c1_c3))*200 + 100, r2_c1_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c2)+1)*50, r2_c2, c="orange")
+plt.plot(np.arange(0, len(r2_c2_c3))*300 + 1000, r2_c2_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3, c="red")
+
+plt.plot([0, 10000], [0, 0], '-k')
+plt.ylim([0, 0.75])
+plt.xscale("log")
+plt.show()
+
+# %% jupyter={"outputs_hidden": true}
+results3 = [get_curves(c1, c2, c3, 0.2, 0.2) for c1,c2,c3 in clients]
+r2_c1, r2_c2, r2_c3, r2_c1_c2, r2_c1_c3, r2_c2_c3 =  (np.mean([np.array(r[i]) for r in results3], axis=0) for i in range(6))
+
+# %%
+plt.plot(np.arange(1, len(r2_c1)+1)*5, r2_c1)
+plt.plot(np.arange(0, len(r2_c1_c2))*50 + 100, r2_c1_c2, '--', c="orange")
+plt.plot(np.arange(0, len(r2_c1_c3))*200 + 100, r2_c1_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c2)+1)*50, r2_c2, c="orange")
+plt.plot(np.arange(0, len(r2_c2_c3))*300 + 1000, r2_c2_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3, c="red")
+
+plt.plot([0, 10000], [0, 0], '-k')
+plt.ylim([0, 0.75])
+plt.xscale("log")
+plt.show()
+
+# %% [markdown]
+# ## Compare with very noisy C3
+
+# %%
+res_noisy  = []
+
+for _ in range(10):
+    c3n = ClientNoiseBoth(3, scaler, records)
+    c3n.init_elm(L, W, bias)
+    c3n.noise_H = 0.2
+    c3n.noise_Y = 0.2
+    r2_c3_noisy = get_optimal_performance(c3n, 300)
+    res_noisy.append(r2_c3_noisy)
+
+r2_c3_noisy = np.mean([np.array(r1) for r1 in res_noisy], axis=0)
+
+# %%
+plt.plot(np.arange(1, len(r2_c1)+1)*5, r2_c1)
+plt.plot(np.arange(0, len(r2_c1_c2))*50 + 100, r2_c1_c2, '--', c="orange")
+plt.plot(np.arange(0, len(r2_c1_c3))*200 + 100, r2_c1_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c2)+1)*50, r2_c2, c="orange")
+plt.plot(np.arange(0, len(r2_c2_c3))*300 + 1000, r2_c2_c3, '--', c="red")
+
+plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3, c="red")
+plt.plot(np.arange(1, len(r2_c3)+1)*300, r2_c3_noisy, ":", c="black")
+
+plt.plot([0, 10000], [0, 0], '-k')
+plt.ylim([0, 0.75])
+plt.xscale("log")
+plt.show()
